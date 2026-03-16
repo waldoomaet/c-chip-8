4e4d63fc41c5a897821186826e85345925b661da
Waldo Ruiz <waldo.omaet6446@gmail.com>
2026-03-16 21:34:00 +0100

    web

--- tools/generate_bin.py
+++
@@ -1,3 +0,0 @@
-code = bytearray([0xe0, 0x9e, 0, 0, 0xe0, 0xa1, 0x12, 0, 0x12, 0])
-with open("./roms/mine.ch8", "wb") as file:
-    file.write(code)
--- tools/hardcode.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-with open(sys.argv[1], 'rb') as file:
-    content = bytearray(file.read())
-    print(f"unsigned char source[] = {{{', '.join(map(hex, content))}}};")
